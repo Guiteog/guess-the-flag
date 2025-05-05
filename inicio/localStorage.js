705f510a4aef26345074ffb36d6aa54642dc4492
Guiteog <gui.tgomez@gmail.com>
2025-05-05 10:27:44 -0300

    Endereço

--- inicio/localStorage.js
+++ inicio/localStorage.js
@@ -3,39 +3,24 @@ let nomeADM = "nomeADM123"
 
 async function dados(){
     localStorage.setItem("Jogador", JSON.stringify({nome:nome.value}));
-
-    //Mandar os dados para o servidor
-    try{
-        let resposta = await fetch("https://2658-200-206-76-106.ngrok-free.app/dados",{
-            method: "POST",
-            headers: {"Content-Type": "application/json" },
-            body:JSON.stringify({nome: nome.value})
-
-        });
-
-        if (!resposta.ok){
-            throw new Error("Erro no envio dos dados");
-        }
-        if(nome.value === nomeADM ){
-            window.location.replace("./meio/indexJogadores.html");
-        }
-
-        else{
-            window.location.replace("./meio/indexJSem.html");
-        }
-        
-
-    } catch(erro){
-        console.error("Erro ao enviar dados:", erro);
-        alert("Não foi possível enviar os dados")
+    try {
+        const resposta = await fetch(`https://2658-200-206-76-106.ngrok-free.app/paises?nome=${encodeURIComponent(nome.value)}`)
+            if(!resposta.ok){
+                throw new Error("Erro na requisição");
+            }
     }
-    
+    catch (erro) {
+        console.error("Erro ao enviar dados:", erro);
+    }
 }
 
-// function jogadores(){
-//     fetch("http://10.106.208.18:1880/dados")
-//         .then(response => response.json())
-//         .then(data => {
+// if (!resposta.ok){
+//     throw new Error("Erro no envio dos dados");
+// }
+// if(nome.value === nomeADM ){
+//     window.location.replace("./meio/indexJogadores.html");
+// }
 
-//         })
+// else{
+//     window.location.replace("./meio/indexJSem.html");
 // }
