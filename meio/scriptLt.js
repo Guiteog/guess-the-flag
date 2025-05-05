c43c2880ef13b19eb8d2bc88221ce2cc3562d10e
Guiteog <gui.tgomez@gmail.com>
2025-05-05 14:43:09 -0300

    com button

--- meio/scriptLt.js
+++ meio/scriptLt.js
@@ -1,12 +1,27 @@
 // Exemplo: adicionando jogadores dinamicamente (pode ser substituído por WebSocket ou fetch API)
 const list = document.getElementById('playersList');
 const startBtn = document.getElementById('startBtn');
+let players;
 
 //pegar dados dos players
+function dados(){
+  fetch("https://2658-200-206-76-106.ngrok-free.app/dados")
+    .then(response => response.json)
+    .then(data =>{
+        players = data;
+        updatePlayerList(players);
+    })
+}
 
 // Simula nova entrada de jogadores
 function updatePlayerList(players) {
   list.innerHTML = ""; // Limpa
+
+  players.forEach(player => {
+    const li = document.createElement("li");
+    li.textContent = player; 
+    list.appendChild(li);
+  });
 
   // Ativa o botão se tiver 3 ou mais jogadores
   if (players.length >= 3) {
@@ -16,5 +31,4 @@ function updatePlayerList(players) {
   }
 }
 
-setInterval(updatePlayerList, 2500)
-updatePlayerList(players);
+setInterval(dados, 2500);
