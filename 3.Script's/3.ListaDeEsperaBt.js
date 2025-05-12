9e9aadd47274d4d3af0387852ef0fb78976bedd2
Guiteog <gui.tgomez@gmail.com>
2025-05-11 23:14:14 -0300

    Serve

--- 3.Script's/3.ListaDeEsperaBt.js
+++ 3.Script's/3.ListaDeEsperaBt.js
@@ -11,7 +11,7 @@ function dados(){
     headers: {
         'ngrok-skip-browser-warning': 'true' 
     }
-})
+  })
     .then(response => response.json())
     .then(data =>{
         players = data;
@@ -39,4 +39,15 @@ function updatePlayerList(players) {
   }
 }
 
+function ordem(){
+  fetch('https://39ae-2804-d43-2b2b-4500-4063-d464-b59c-c5a2.ngrok-free.app', {
+    method: 'GET',
+    headers: {
+        'ngrok-skip-browser-warning': 'true' 
+    }
+  })
+
+  
+
+}
 setInterval(dados, 2500);
