9715182414ac5798062424fb5d2132ef86de62b9
Guiteog <gui.tgomez@gmail.com>
2025-05-12 13:46:51 -0300

    sinal

--- 3.Script's/3.ListaDeEsperaBt.js
+++ 3.Script's/3.ListaDeEsperaBt.js
@@ -56,14 +56,13 @@ function s(){
     }
   })
 
-    .then(response => response.json())
-    .then(data =>{
-      if (data.sinal === true){
-        sinal = data.sinal
-      }
-      else{
-        sinal = false
-      }
+  .then(response => response.json())
+  .then(data => {
+    if (data && data.sinal === true) {
+      sinal = true;
+    } else {
+      sinal = false;
+    }
   })
 }
 
