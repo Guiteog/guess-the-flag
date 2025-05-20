2368da13e689c35f8d66773f229519fc0a6523d0
MeriG123 <gabriela.giacomini@aluno.senai.br>
2025-05-20 17:23:00 -0300

    Add files via upload

---
+++ 2.ListaDeEspera.js
@@ -0,0 +1,48 @@
+// Exemplo: adicionando jogadores dinamicamente (pode ser substituído por WebSocket ou fetch API)
+const list = document.getElementById('playersList');
+const startBtn = document.getElementById('startBtn');
+let players;
+let sinal = false;
+
+//Link wss
+const wsRespostaURL ="wss://f04a-200-211-208-194.ngrok-free.app/ws/resposta";//Sinal para a troca de pagina
+const wsJogadoresURL ="wss://f04a-200-211-208-194.ngrok-free.app/ws/retornadados";//Sinal para coletar
+
+// Criando conexões WebSocket
+const wsResposta = new WebSocket(wsRespostaURL);
+const wsJogadores = new WebSocket(wsJogadoresURL);
+
+//pegar dados dos players
+wsJogadores.onmessage = (event) => {
+  players = JSON.parse(event.data);
+  updatePlayerList(players)
+}
+
+//Espera do sinal
+wsResposta.onmessage = (event) => {
+  sinalJSON = JSON.parse(event.data)
+  sinal = sinalJSON.sinal
+}
+
+
+// Adiciona 
+function updatePlayerList(players) {
+  list.innerHTML = ""; // Limpa
+
+  console.log(players);
+
+  players.forEach(player => {
+    const li = document.createElement("li");
+    li.textContent = player.nome; 
+    list.appendChild(li);
+  });
+}
+
+
+
+setInterval(() => {
+  if (sinal) {
+    window.location.replace("../1.Espera-Jogo/jogo.html");
+    
+  }
+}, 250);
---
+++ script010101.js
@@ -0,0 +1,30 @@
+let players;
+let rank = document.getElementById ("rank")
+
+//Link wss
+const wsJogadoresURL ="wss://f04a-200-211-208-194.ngrok-free.app/ws/retornadados";//Sinal para coletar
+
+// Criando conexões WebSocket
+const wsJogadores = new WebSocket(wsJogadoresURL);
+
+//pegar dados dos players
+
+wsJogadores.onmessage = (event) => {
+  players = JSON.parse(event.data);
+  updatePlayerList(players)
+}
+
+
+// Adiciona 
+function updatePlayerList(players) {
+  list.innerHTML = ""; // Limpa
+
+  console.log(players);
+
+  players.forEach(player => {
+    const li = document.createElement("li");
+    li.textContent = player.nome; 
+    list.appendChild(li);
+  });
+}
+ 
