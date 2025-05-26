ada3d20decdbe1df2aea3bfa9b4c6bf26276c3ce
Guiteog <gui.tgomez@gmail.com>
2025-05-26 10:04:32 -0300

    Rank

--- 3.Script's/5.RankList.js
+++ 3.Script's/5.RankList.js
@@ -1,35 +1,30 @@
+let players;
+let rank = document.getElementById ("rank")
 
-async  function exibirResultados() {
-  try {
-    const resposta = await fetch(`https://f04a-200-211-208-194.ngrok-free.app/dados?nome=${nome.value}`, {
-        method: 'GET',
-        headers: {
-            'ngrok-skip-browser-warning': 'true' 
-        }
-    })
-        if(!resposta.ok){
-            throw new Error("Erro na requisição");
-        }
+//Link wss
+const wsJogadoresURL ="wss://f04a-200-211-208-194.ngrok-free.app/ws/retornadados";//Sinal para coletar
 
-        if(nome.value === nomeADM ){
-            window.location.replace("../1.Espera-Jogo/indexEspera.html");
-        }
-       
-        else{
-            window.location.replace("../1.Espera-Jogo/indexEsperaSem.html");
-        }
-}
-  catch (erro) {
-    console.error("Erro ao enviar dados:", erro);
-}
-  
-  
+// Criando conexões WebSocket
+const wsJogadores = new WebSocket(wsJogadoresURL);
+
+//pegar dados dos players
+
+wsJogadores.onmessage = (event) => {
+  players = JSON.parse(event.data);
+  updatePlayerList(players)
 }
 
-function voltarMenu() {
-  // Substitua com o redirecionamento real do seu jogo
-  window.location.replace("../index.html");
-  
-}
 
-exibirResultados()
+// Adiciona 
+function updatePlayerList(players) {
+  list.innerHTML = ""; // Limpa
+
+  console.log(players);
+
+  players.forEach(player => {
+    const li = document.createElement("li");
+    li.textContent = player.nome; 
+    list.appendChild(li);
+  });
+}
+ 
