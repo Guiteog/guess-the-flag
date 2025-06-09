50a8cd97dfaad51592766cd4a328a55008b43fcf
Guiteog <gui.tgomez@gmail.com>
2025-06-09 09:16:49 -0300

    listasem

--- 3.Script's/2.ListaDeEspera.js
+++ 3.Script's/2.ListaDeEspera.js
@@ -7,16 +7,24 @@ let sinal = false;
 //Link wss
 const wsRespostaURL ="wss://1523-200-206-76-106.ngrok-free.app/ws/resposta";//Sinal para a troca de pagina
 const wsJogadoresURL ="wss://1523-200-206-76-106.ngrok-free.app/ws/retornadados";//Sinal para coletar
+const wslistaURL ="wss://1523-200-206-76-106.ngrok-free.app/ws/lista"; //Lista sempre atualizada
 
 // Criando conexões WebSocket
 const wsResposta = new WebSocket(wsRespostaURL);
 const wsJogadores = new WebSocket(wsJogadoresURL);
+const wsLista = new WebSocket(wslistaURL);
 
 //pegar dados dos players
 wsJogadores.onmessage = (event) => {
   players = JSON.parse(event.data);
   updatePlayerList(players)
 }
+
+wsLista.onmessage = (event) => {
+  players = JSON.parse(event.data);
+  updatePlayerList(players)
+};
+
 
 //sinal para pegar os dados
 wsJogadores.onopen = () => {
